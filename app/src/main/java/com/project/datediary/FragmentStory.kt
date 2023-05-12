2de06eab90c12a82e1692a796ecab2ffb0b9f599
CampbellBiology <urmapw@naver.com>
2023-05-12 15:57:48 +0900

    asdf

--- app/src/main/java/com/project/datediary/FragmentStory.kt
+++ app/src/main/java/com/project/datediary/FragmentStory.kt
@@ -5,7 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import com.bumptech.glide.Glide
+import com.project.datediary.databinding.FragmentHomeBinding
 import com.project.datediary.databinding.FragmentStoryBinding
 
 class FragmentStory : Fragment() {
@@ -18,7 +19,29 @@ class FragmentStory : Fragment() {
     ): View? {
         binding = FragmentStoryBinding.inflate(inflater, container, false)
 
+//        Glide.with(this)
+//            .load(R.drawable.icon_story)
+//            .into(binding.imageview)
+
+
+        binding.button1.setOnLongClickListener {
+            childFragmentManager.beginTransaction()
+                .replace(R.id.editContainer, FragmentStoryEdit())
+                .commit()
+            return@setOnLongClickListener(true)
+        }
+
+//        binding.button1.setOnClickListener {
+//
+//            Glide.with(this)
+//                .load(R.drawable.icon_story)
+//                .into(binding.imageview)
+//
+//        }
+
         return binding.root
     }
+
+
 
 }
