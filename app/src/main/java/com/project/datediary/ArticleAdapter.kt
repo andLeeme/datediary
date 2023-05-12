d5e748bf702f61ac81f81cabc925ac19c3ba629b
JH <rarara773@gmail.com>
2023-05-12 15:45:39 +0900

    23.05.12 15:45 Commit.JH

---
+++ app/src/main/java/com/project/datediary/ArticleAdapter.kt
@@ -0,0 +1,51 @@
+package com.project.datediary
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.project.datediary.databinding.ItemListBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+class ArticleAdapter : ListAdapter<ArticleModel, ArticleAdapter.ViewHolder>(diffUtil){
+    inner class ViewHolder (private val binding: ItemListBinding): RecyclerView.ViewHolder(binding.root){
+        fun bind(articleModel: ArticleModel){
+            val format = SimpleDateFormat("MM년dd일")
+            val date = Date(articleModel.createdAt)
+
+
+
+            binding.text01.text = articleModel.title
+            binding.text02.text = format.format(date).toString()
+            binding.text03.text = articleModel.price
+            if(articleModel.imageUrl.isNotEmpty()){
+                Glide.with(binding.thumbnailImageView)
+                    .load(articleModel.imageUrl)
+                    .into(binding.thumbnailImageView)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(ItemListBinding.inflate(LayoutInflater.from(parent.context),parent,false))
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(currentList[position])
+    }
+
+    companion object{
+        val diffUtil = object  : DiffUtil.ItemCallback<ArticleModel>(){
+            override fun areItemsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
+                return oldItem.createdAt == newItem.createdAt
+            }
+
+            override fun areContentsTheSame(oldItem: ArticleModel, newItem: ArticleModel): Boolean {
+                return oldItem == newItem
+            }
+        }
+    }
+}
