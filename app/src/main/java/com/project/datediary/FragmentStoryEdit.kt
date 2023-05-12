5e9dd08373c82151c3d6aace178596ea23fb835a
CampbellBiology <urmapw@naver.com>
2023-05-12 15:59:25 +0900

    asdf

---
+++ app/src/main/java/com/project/datediary/FragmentStoryEdit.kt
@@ -0,0 +1,25 @@
+package com.project.datediary
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.project.datediary.databinding.FragmentStoryEditBinding
+
+class FragmentStoryEdit : Fragment() {
+
+    lateinit var binding: FragmentStoryEditBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentStoryEditBinding.inflate(inflater, container, false)
+
+        //편집기능 넣기
+
+        return binding.root
+    }
+
+}
