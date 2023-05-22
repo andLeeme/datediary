9b69188c42972408bb8bb0eac50377e965906823
jihyun <rarara773@gmail.com>
2023-05-23 00:03:43 +0900

    05.23_1 commit

--- app/src/main/java/com/project/datediary/fragment/FragmentStaggeredGrid.kt
+++ app/src/main/java/com/project/datediary/fragment/FragmentStaggeredGrid.kt
@@ -1,21 +1,17 @@
 package com.project.datediary.fragment
 
-import android.content.ClipData
-import android.content.ClipDescription
-import android.content.Context
-import android.os.Build
 import android.os.Bundle
-import android.view.DragEvent
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.ItemTouchHelper
-import com.project.datediary.databinding.FragmentStaggeredGridBinding
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import com.project.datediary.R
+import com.project.datediary.adapter.MyAdapter
+import com.project.datediary.databinding.FragmentStaggeredGridBinding
+import com.project.datediary.databinding.FragmentStoryBinding
+import java.util.Collections
 
 
 class FragmentStaggeredGrid : Fragment() {
@@ -23,8 +19,6 @@ class FragmentStaggeredGrid : Fragment() {
     lateinit var binding: FragmentStaggeredGridBinding
 
 
-    private lateinit var recyclerView: RecyclerView
-    private lateinit var adapter: ItemAdapter
 
 
     override fun onCreateView(
@@ -32,149 +26,61 @@ class FragmentStaggeredGrid : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        recyclerView = binding.recyclerView
-
         binding = FragmentStaggeredGridBinding.inflate(inflater, container, false)
 
+        val recyclerView = binding.recyclerView
 
-        val layoutManager = StaggeredGridLayoutManager(2, RecyclerView.VERTICAL)
+
+        val layoutManager = StaggeredGridLayoutManager(
+            3, // 열의 수
+            StaggeredGridLayoutManager.VERTICAL // 방향
+        )
         recyclerView.layoutManager = layoutManager
 
-        // 아이템 데이터 생성
-        val items = mutableListOf<String>()
-        for (i in 1..20) {
-            items.add("Item $i")
+        val itemList = mutableListOf<String>()
+
+        for (i in 1..35) {
+            (itemList as MutableList<String>).add("Item $i")
         }
 
-        // 어댑터 설정
-        adapter = ItemAdapter(requireContext(), items)
+        val adapter = MyAdapter(itemList)
         recyclerView.adapter = adapter
 
-        // 아이템 터치 헬퍼 설정
-        val itemTouchHelper = ItemTouchHelper(ItemTouchHelperCallback(adapter))
+        val itemTouchHelper = ItemTouchHelper(object : ItemTouchHelper.Callback() {
+            override fun getMovementFlags(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder
+            ): Int {
+                val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN or
+                        ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
+                return makeMovementFlags(dragFlags, 0)
+            }
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                val fromPosition = viewHolder.adapterPosition
+                val toPosition = target.adapterPosition
+
+                // 아이템 위치 변경
+                Collections.swap(itemList, fromPosition, toPosition)
+                adapter.notifyItemMoved(fromPosition, toPosition)
+
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // 스와이프 동작 처리
+            }
+        })
         itemTouchHelper.attachToRecyclerView(recyclerView)
+
 
         return binding.root
     }
 
-    inner class ItemAdapter(private val context: Context, private val items: List<String>) :
-        RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-            val view =
-                LayoutInflater.from(context).inflate(R.layout.item_view, parent, false)
-            return ItemViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-            val item = items[position]
-            holder.textView.text = item
-
-            // 드래그 앤 드롭 이벤트 처리
-            holder.itemView.setOnLongClickListener { v ->
-                val clipText = ClipData.newPlainText("text", item)
-                val shadowBuilder = View.DragShadowBuilder(v)
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    v.startDragAndDrop(
-                        clipText,
-                        shadowBuilder,
-                        v,
-                        0
-                    )
-                } else {
-                    v.startDrag(
-                        clipText,
-                        shadowBuilder,
-                        v,
-                        0
-                    )
-                }
-            }
-
-            holder.itemView.setOnDragListener(DragListener())
-        }
-
-        override fun getItemCount(): Int {
-            return items.size
-        }
-
-        inner class ItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-            val textView: TextView = itemView.findViewById(R.id.textView)
-        }
-    }
-
-    inner class ItemTouchHelperCallback(private val adapter: ItemAdapter) :
-        ItemTouchHelper.Callback() {
-
-        override fun getMovementFlags(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder
-        ): Int {
-            val dragFlags = ItemTouchHelper.UP or ItemTouchHelper.DOWN or ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT
-            return makeMovementFlags(dragFlags, 0)
-        }
-
-        override fun onMove(
-            recyclerView: RecyclerView,
-            viewHolder: RecyclerView.ViewHolder,
-            target: RecyclerView.ViewHolder
-        ): Boolean {
-            adapter.notifyItemMoved(viewHolder.adapterPosition, target.adapterPosition)
-            return true
-        }
-
-        override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
-            // 스와이프 동작 처리
-        }
-
-        override fun isLongPressDragEnabled(): Boolean {
-            return true
-        }
-    }
-
-    inner class DragListener : View.OnDragListener {
-
-        override fun onDrag(v: View, event: DragEvent): Boolean {
-            val action = event.action
-            when (action) {
-                DragEvent.ACTION_DRAG_STARTED -> {
-                    if (event.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)) {
-                        v.setBackgroundColor(resources.getColor(R.color.black))
-                        v.invalidate()
-                        return true
-                    }
-                    return false
-                }
-                DragEvent.ACTION_DRAG_ENTERED -> {
-                    v.setBackgroundColor(resources.getColor(R.color.black))
-                    v.invalidate()
-                    return true
-                }
-                DragEvent.ACTION_DRAG_LOCATION -> {
-                    return true
-                }
-                DragEvent.ACTION_DRAG_EXITED -> {
-                    v.setBackgroundColor(resources.getColor(android.R.color.white))
-                    v.invalidate()
-                    return true
-                }
-                DragEvent.ACTION_DROP -> {
-                    val item = event.clipData.getItemAt(0).text.toString()
-                    val targetView = v as TextView
-                    targetView.text = item
-                    v.setBackgroundColor(resources.getColor(android.R.color.white))
-                    v.invalidate()
-                    return true
-                }
-                DragEvent.ACTION_DRAG_ENDED -> {
-                    v.setBackgroundColor(resources.getColor(android.R.color.white))
-                    v.invalidate()
-                    return true
-                }
-                else -> return false
-            }
-        }
-
-    }
 }
 
