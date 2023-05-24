66f633324103d617cc12acd5637aeb08974509fd
JH <rarara773@gmail.com>
2023-05-24 10:07:44 +0900

    0524-1 commit

--- app/src/main/java/com/project/datediary/model/dataVO.kt
+++ app/src/main/java/com/project/datediary/model/dataVO.kt
@@ -1,3 +1,3 @@
 package com.project.datediary.model
 
-class dataVO(val sche1: String, val sche2: String, val sche3: String, val sche4: String)
+data class dataVO(val sche1: String, val sche2: String, val sche3: String, val sche4: String)
