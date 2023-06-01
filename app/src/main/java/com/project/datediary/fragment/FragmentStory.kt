ff554042a8114e8fd2aec1c4e0f29a31f982a059
JH <rarara773@gmail.com>
2023-06-01 19:17:24 +0900

    0601-4 commit

--- app/src/main/java/com/project/datediary/fragment/FragmentStory.kt
+++ app/src/main/java/com/project/datediary/fragment/FragmentStory.kt
@@ -6,8 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.project.datediary.activity.AddScheduleActivity2
 import com.project.datediary.R
+import com.project.datediary.activity.AddScheduleActivity2
+import com.project.datediary.activity.EditScheduleActivity
 import com.project.datediary.databinding.ActivityMainBinding
 import com.project.datediary.databinding.FragmentStoryBinding
 
